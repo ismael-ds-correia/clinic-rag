578a05b71038762e40b8c4c07777f0a9e29fdb43
CarlosNeto-dev <netoc4533@gmail.com>
2026-07-08 19:02:20 -0300

    Features: New Embeddings & Requirements.txt

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "🚀 Starting the Jupyter Notebook & Streamlit Ambient..."
+echo "🚀 Setup successfully created!"
 
 # streamlit run academic/src/app.py --server.port=8501 --server.address=0.0.0.0 &
-exec jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''
+
+exec tail -f /dev/null
